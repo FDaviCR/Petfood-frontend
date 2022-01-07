46abc429a2806adf503b7b9e020d5b4cff5b187e
Davi <davicamiloribeiro@gmail.com>
2022-01-06 21:41:32 -0300

    Construção de tela de cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,5 +1,53 @@
+import Logo from '../../assets/logo-white.png';
+import Illustration from '../../assets/illustration.svg';
+
 const Cadastro = () => {
-    return <div className="container-fluid h-100 bg-primary"></div>
+    return (
+        <div className="container-fluid h-100 bg-primary">
+            <div className="col-12">
+                <img src={Logo} alt="" />
+            </div>
+            <div className="row">
+                <div className="col-6 text-right my-auto">
+                    <img width="100%" className="img-fluid" src={Illustration} alt="" />
+                </div>
+                <div className="col-6">
+                    <div className="box col-8">
+                        <h2 className="text-center">Falta pouco para fazer o seu pet feliz.</h2>
+                        <br />
+                        <input 
+                            type="text" 
+                            className="form-control form-control-lg mb-3"
+                            placeholder='Nome completo'    
+                        />
+                        <input 
+                            type="email" 
+                            className="form-control form-control-lg mb-3"
+                            placeholder='E-mail'    
+                        />
+                        <input 
+                            type="text" 
+                            className="form-control form-control-lg mb-3"
+                            placeholder='Telefone'    
+                        />
+                        <input 
+                            type="text"
+                            className="form-control form-control-lg mb-3"
+                            placeholder='CPF'    
+                        />
+                        <input 
+                            type="date" 
+                            className="form-control form-control-lg mb-3"
+                            placeholder='Data de nascimento'    
+                        />
+                        <button className="btn btn-pedido btn-lg btn-block btn-secondary">
+                            Finalizar Pedido
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Cadastro;
