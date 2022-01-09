93ac61574810eee11de78a971a6b28d838114102
Davi <davicamiloribeiro@gmail.com>
2022-01-08 23:44:38 -0300

    Tela de chackout - base

---
+++ src/pages/checkout/index.js
@@ -0,0 +1,11 @@
+import Header from '../../components/header';
+
+const Checkout = () => {
+    return (
+        <div className="h-100">
+            <Header />
+        </div>
+    );
+};
+
+export default Checkout;
